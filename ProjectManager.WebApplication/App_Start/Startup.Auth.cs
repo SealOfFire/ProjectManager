372fb6a1b90bed2ba5396057997167323a6666fc
xus <sealoffire@hotmail.com>
2018-12-20 16:08:05 +0800

    添加owin

---
+++ ProjectManager.WebApplication/App_Start/Startup.Auth.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace ProjectManager.WebApplication.App_Start
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
---
+++ ProjectManager.WebApplication/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using ProjectManager.Log;
+
+[assembly: OwinStartup(typeof(ProjectManager.WebApplication.Startup))]
+namespace ProjectManager.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Logger.Info("owin startup");
+            this.Configuration(app);
+        }
+    }
+}
